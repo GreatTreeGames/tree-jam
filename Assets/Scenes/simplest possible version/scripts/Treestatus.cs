58dbd75055a68ee369282022c3f8b7ad7141f22d
Jackson Fielder <greattreegames@gmail.com>
2023-02-04 01:32:20 -0500

    textmeshpro with user interface and graphics updater no work

--- Assets/Scenes/simplest possible version/scripts/Treestatus.cs
+++ Assets/Scenes/simplest possible version/scripts/Treestatus.cs
@@ -10,48 +10,57 @@ public class Treestatus : MonoBehaviour
     public void rushRoots()
     {
         status.roots.width+=2;
+        visual.updateGraphics(status);
     }
 
     public void boomRoots()
     {
         status.roots.width++;
         status.roots.height++;
+        visual.updateGraphics(status);
     }
 
     public void turtleRoots()
     {
         status.roots.height+=2;
+        visual.updateGraphics(status);
     }
 
     public void RushTrunk()
     {
         //wow it's fucking nothing
+        visual.updateGraphics(status);
     }
 
     public void boomTrunk()
     {
         status.trunk.height+=2;
+        visual.updateGraphics(status);
     }
 
     public void turtleTrunk()
     {
         status.trunk.width +=2;
+        visual.updateGraphics(status);
     }
 
     public void rushBranchesLeaves()
     {
         status.branchesLeaves.wideness+=2;
+        visual.updateGraphics(status);
     }
 
     public void boomBranchesLeaves()
     {
         status.branchesLeaves.height+= 1;
         status.branchesLeaves.wideness+=1;
+        visual.updateGraphics(status);
     }
 
     public void turtleBranchesLeaves()
     {
         status.branchesLeaves.darkness+=2;
+        visual.updateGraphics(status);
     }
 
 
