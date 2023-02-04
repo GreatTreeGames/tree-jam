70a3885311594153a04143431cf53d4536a402d2
Jackson Fielder <greattreegames@gmail.com>
2023-02-03 23:39:28 -0500

    initial commit

---
+++ Assets/Scenes/simplest possible version/scripts/Treestatus.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treestatus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public struct Statuses
+{
+
+}
+
+public struct rootsStatus
+{
+
+}
+
+public struct trunkStatus
+{
+
+}
+
+public struct branchesLeavesStatus
+{
+
+}
