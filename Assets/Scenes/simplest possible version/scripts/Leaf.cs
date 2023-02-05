9c57776aaed4ee94515ab1da53c91bf00c22d0e0
Logan Petruziello <loganpetro97@yahoo.com>
2023-02-04 20:45:18 -0500

    leaf i guess

---
+++ Assets/Scenes/simplest possible version/scripts/Leaf.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO[LP]: piss pants
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
